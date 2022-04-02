d03889a888fd4059b71ecbbe524193033792c72a
Wolfgang Fahl <wf@bitplan.com>
2022-04-02 14:57:44 +0200

    fixes setup

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(name=Version.name,
       ),
       classifiers=[
             'Programming Language :: Python',
-            'Programming Language :: Python :: 3.6'
+            'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
